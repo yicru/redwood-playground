a7938e79f7a6c9b00a6e73ce55f9b214d9058734
yuichiro miyamae <yu1ro.mya@gmail.com>
2022-06-02 21:52:33 +0900

    add: routing

--- web/src/pages/AboutPage/AboutPage.tsx
+++ web/src/pages/AboutPage/AboutPage.tsx
@@ -7,13 +7,15 @@ const AboutPage = () => {
       <MetaTags title="About" description="About page" />
 
       <h1>AboutPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/AboutPage/AboutPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>about</code>, link to me with `
-        <Link to={routes.about()}>About</Link>`
-      </p>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.home()}>Home</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </>
   )
 }
